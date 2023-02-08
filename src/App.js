74418782f00ca63a1f09d6a61f0e90ed00e35d81
Andres Alba <tensinkusum@example.com>
2023-02-08 11:39:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [ans, setAns] = useState('') 
+
+  const apiTest = () => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=1`)
+    .then((datos)=>{ return datos.json(); })
+    .then((resp)=>{ console.log(resp.results); 
+                    setAns(JSON.stringify(resp.results));
+                  })
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={apiTest} >Query</button>
+      <p>{ans}</p>
     </div>
   );
 }
